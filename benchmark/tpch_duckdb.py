eac6d270f9c12cbb1dcd23d60ad3ebf1346f3384
haok1402 <haok1402@gmail.com>
2025-04-11 00:19:55 -0400

    revise the code, to test multiple times, with "warm up"!

---
+++ benchmark/tpch_duckdb.py
@@ -0,0 +1,34 @@
+import os
+import tempfile
+import logging
+import subprocess
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(message)s")
+
+template = """
+SET threads TO {};
+PRAGMA enable_profiling;
+SET enable_profiling = 'json';
+SET profiling_output = '{}.json';
+
+SELECT l_orderkey, COUNT(*), SUM(l_extendedprice), AVG(l_discount) FROM lineitem GROUP BY l_orderkey;
+SELECT l_orderkey, COUNT(*), SUM(l_extendedprice), AVG(l_discount) FROM lineitem GROUP BY l_orderkey;
+SELECT l_orderkey, COUNT(*), SUM(l_extendedprice), AVG(l_discount) FROM lineitem GROUP BY l_orderkey;
+SELECT l_orderkey, COUNT(*), SUM(l_extendedprice), AVG(l_discount) FROM lineitem GROUP BY l_orderkey;
+SELECT l_orderkey, COUNT(*), SUM(l_extendedprice), AVG(l_discount) FROM lineitem GROUP BY l_orderkey;
+"""
+
+def main():
+    for database in Path("data").glob("tpch-sf*.db"):
+        for threads in range(os.cpu_count(), os.cpu_count() + 1):
+            outfile = Path("report", database.stem, "duckdb", str(threads))
+            outfile.parent.mkdir(parents=True, exist_ok=True)
+            logging.info(f"Running query on {database} with {threads} threads.")
+            with tempfile.NamedTemporaryFile() as file:
+                file.write(template.format(threads, outfile).encode()); file.flush()
+                kwargs = dict(check=True, shell=True, stdout=subprocess.DEVNULL)
+                subprocess.run(f"duckdb {database} < {file.name}", **kwargs)
+
+if __name__ == "__main__":
+    main()
--- data/prepare.sh
+++ data/prepare.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Download TPC-H datasets (SF1/10/100) from DuckDB-hosted links.
-
-# Author: Hao Kang
-# Date: April 8, 2025
 
 for scale in 1 10 100; do
     wget -P data/ "https://blobs.duckdb.org/data/tpch-sf${scale}.db"
