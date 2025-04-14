75493fd1c4fdfa7a4f4b0efb3f62bf243fe96593
haok1402 <haok1402@gmail.com>
2025-04-14 09:59:31 -0400

    able to iterate the result after reading the database file from duckdb

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,19 @@ int main()
     duckdb::DuckDB db("data/tpch-sf1.db");
     duckdb::Connection con(db);
 
-    auto result = con.Query("SELECT COUNT(*) FROM lineitem;");
+    auto result = con.Query("SELECT * FROM lineitem;");
     if (result->HasError())
     {
         std::cerr << result->GetError() << std::endl;
+        return 1;
     }
-    else
-    {
-        std::cout << result->ToString() << std::endl;
+
+    while (auto chunk = result->Fetch()) {
+        std::cout << "chunk->size() = " << chunk->size() << std::endl;
+        std::cout << "chunk->ColumnCount() = " << chunk->ColumnCount() << std::endl;
     }
+
+    std::cout << "result->RowCount() = " << result->RowCount() << std::endl;
 
     return 0;
 }
