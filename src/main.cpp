d47f4d77a9350806e90da3d94d2f53bb9dff5e29
haok1402 <haok1402@gmail.com>
2025-04-14 16:23:40 -0400

    let's fetch the data into row-major format to be ready for the groupby queries!!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,34 @@
 
 int main()
 {
-    /**
-     * 1. Read the database
-     */
-    duckdb::DuckDB db("data/tpch-sf1.db");
+    std::string tname = "lineitem";
+    std::string fpath = "data/tpch-sf1.db";
+    std::vector<std::string> keys = {"l_orderkey"};
+    std::vector<std::string> vals = {"l_extendedprice", "l_discount"};
+    std::vector<std::string> funs = {"SUM", "AVG"};
+
+    duckdb::DuckDB db(fpath);
     duckdb::Connection con(db);
 
-    std::string query = "SELECT l_orderkey, COUNT(*), SUM(l_extendedprice), AVG(l_discount) FROM lineitem GROUP BY l_orderkey";
+    auto table_description = con.TableInfo(tname);
+    for (auto& column_description : table_description->columns)
+    {
+        auto name = column_description.GetName();
+        if ((std::find(keys.begin(), keys.end(), name) == keys.end()) && (std::find(vals.begin(), vals.end(), name) == vals.end()))
+        {
+            continue;
+        }
+        auto type = column_description.GetType();
+        if (type.id() != duckdb::LogicalTypeId::BIGINT && type.id() != duckdb::LogicalTypeId::DECIMAL)
+        {
+            throw std::runtime_error("Unsupported column type: expected BIGINT or DECIMAL, but got " + type.ToString());
+        }
+        std::cout << name << std::endl;
+    }
 
-    auto root = con.ExtractPlan(query);
-    std::cout << root->ToString() << std::endl;
+    std::vector<std::vector<duckdb::Value>> data;
+    auto result = con.Query("SELECT * FROM " + tname);
+    data.reserve(result->RowCount());
 
     return 0;
 }
