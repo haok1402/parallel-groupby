f679d8ae27437d1000467db13ba576463ac12865
haok1402 <haok1402@gmail.com>
2025-04-14 09:23:12 -0400

    revise the setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Prepare the development environment.
 
 # Author: Hao Kang
 # Date: April 14, 2025
 
+# Prepare the development environment.
 wget https://github.com/duckdb/duckdb/releases/download/v1.2.2/libduckdb-linux-amd64.zip
 unzip libduckdb-linux-amd64.zip && rm libduckdb-linux-amd64.zip
 
@@ -12,3 +12,8 @@ mv duckdb.h duckdb.hpp lib/duckdb/include/
 
 mkdir -p lib/duckdb/lib
 mv libduckdb.so libduckdb_static.a lib/duckdb/lib/
+
+# Download the dataset for benchmarking purposes.
+wget -P data/ https://blobs.duckdb.org/data/tpch-sf1.db
+wget -P data/ https://blobs.duckdb.org/data/tpch-sf10.db
+wget -P data/ https://blobs.duckdb.org/data/tpch-sf100.db
