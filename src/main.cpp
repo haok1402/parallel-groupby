c83f56c40c2186230d0d87b23d4ea4bb03f3032b
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-04-15 08:23:49 -0400

    merge and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,33 @@
 #include <chrono>
-#include <iostream>
 #include <duckdb.hpp>
+#include <iostream>
 
-struct Entry
-{
+struct Entry {
     int64_t l_orderkey;
     int64_t l_partkey;
     int64_t l_suppkey;
 };
 
-int main()
-{
-    std::string tname = "lineitem";
-    std::string fpath = "data/tpch-sf1.db";
-    std::vector<std::string> keys = {"l_orderkey"};
-    std::vector<std::string> vals = {"l_partkey", "l_suppkey"};
+int main() {
+    std::string table_name = "lineitem";
+    std::string db_fpath = "data/tpch-sf1.db";
+    std::vector<std::string> group_key_cols = {"l_orderkey"};
+    std::vector<std::string> val_cols = {"l_partkey", "l_suppkey"};
 
-    duckdb::DuckDB db(fpath);
+    duckdb::DuckDB db(db_fpath);
     duckdb::Connection con(db);
 
     /**
-     * Populate the data in row-major format with limited columns.
-     */
+    * Populate the data in row-major format with limited columns.
+    */
     std::vector<Entry> data;
-    auto result = con.Query("SELECT l_orderkey,l_partkey,l_suppkey FROM lineitem");
+    auto result =
+    con.Query("SELECT l_orderkey,l_partkey,l_suppkey FROM lineitem");
     data.reserve(result->RowCount());
 
-    while (auto chunk = result->Fetch())
-    {
-        for (duckdb::idx_t i = 0; i < chunk->size(); i++)
-        {
-            Entry entry
-            {
+    while (auto chunk = result->Fetch()) {
+        for (duckdb::idx_t i = 0; i < chunk->size(); i++) {
+            Entry entry{
                 chunk->GetValue(0, i).GetValue<int64_t>(),
                 chunk->GetValue(1, i).GetValue<int64_t>(),
                 chunk->GetValue(2, i).GetValue<int64_t>(),
@@ -43,23 +39,21 @@ int main()
     auto t0 = std::chrono::high_resolution_clock::now();
 
     /**
-     * Run the single-threaded version of groupby.
-     */
-    std::unordered_map<int64_t, Entry> record;
-    for (auto &row : data)
-    {
+    * Run the single-threaded version of groupby.
+    */
+    std::unordered_map<int64_t, Entry> agg_map;
+    for (auto &row : data) {
         /**
-         * Lookup the previous value.
-         */
+        * Lookup the previous value.
+        */
         Entry prev;
-        if (auto search = record.find(row.l_orderkey); search != record.end())
-        {
+        if (auto search = agg_map.find(row.l_orderkey); search != agg_map.end()) {
             prev = search->second;
         }
         /**
-         * Do the aggregation. WARNING: SUM for now.
-         */
-        record[row.l_orderkey] = prev;
+        * Do the aggregation. WARNING: SUM for now.
+        */
+        agg_map[row.l_orderkey] = prev;
         prev.l_orderkey = row.l_orderkey;
         prev.l_partkey += row.l_partkey;
         prev.l_suppkey += row.l_suppkey;
