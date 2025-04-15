7c7661f454ff477d3f9ea0b3f8774aeacafb6731
haok1402 <haok1402@gmail.com>
2025-04-14 23:47:18 -0400

    use vector of struct instead of vector of vector and the nasty crazy redundant type casting with duckdb

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,11 @@
 #include <iostream>
 #include <duckdb.hpp>
 
-struct ValueHash
+struct Entry
 {
-    std::size_t operator()(const duckdb::Value& value) const
-    {
-        return value.Hash();
-    }
+    int64_t l_orderkey;
+    int64_t l_partkey;
+    int64_t l_suppkey;
 };
 
 int main()
@@ -16,62 +15,28 @@ int main()
     std::string fpath = "data/tpch-sf1.db";
     std::vector<std::string> keys = {"l_orderkey"};
     std::vector<std::string> vals = {"l_partkey", "l_suppkey"};
-    std::vector<std::string> funs = {"SUM", "SUM"};
-
-    /**
-     * For the purpose of illustration, let's support SUM only.
-     */
-    for (auto& f : funs)
-    {
-        if (f != "SUM")
-        {
-            throw std::runtime_error("Unsupported aggregate function: expected SUM, but got " + f);
-        }
-    }
 
     duckdb::DuckDB db(fpath);
     duckdb::Connection con(db);
 
     /**
-     * Determine the columns required for execution.
-     */
-    std::vector<duckdb::idx_t> cols;
-    auto table_description = con.TableInfo(tname);
-    for (duckdb::idx_t i = 0; i < table_description->columns.size(); i++)
-    {
-        auto& column_description = table_description->columns[i];
-
-        auto name = column_description.GetName();
-        if ((std::find(keys.begin(), keys.end(), name) == keys.end()) && (std::find(vals.begin(), vals.end(), name) == vals.end()))
-        {
-            continue;
-        }
-        auto type = column_description.GetType();
-        if (type.id() != duckdb::LogicalTypeId::BIGINT && type.id() != duckdb::LogicalTypeId::DECIMAL)
-        {
-            throw std::runtime_error("Unsupported column type: expected BIGINT, but got " + type.ToString());
-        }
-
-        cols.push_back(i);
-    }
-
-    /**
      * Populate the data in row-major format with limited columns.
      */
-    std::vector<std::vector<duckdb::Value>> data;
-    auto result = con.Query("SELECT * FROM " + tname);
+    std::vector<Entry> data;
+    auto result = con.Query("SELECT l_orderkey,l_partkey,l_suppkey FROM lineitem");
     data.reserve(result->RowCount());
 
     while (auto chunk = result->Fetch())
     {
         for (duckdb::idx_t i = 0; i < chunk->size(); i++)
         {
-            std::vector<duckdb::Value> temp;
-            for (duckdb::idx_t j : cols)
+            Entry entry
             {
-                temp.push_back(chunk->GetValue(j, i));
-            }
-            data.push_back(temp);
+                chunk->GetValue(0, i).GetValue<int64_t>(),
+                chunk->GetValue(1, i).GetValue<int64_t>(),
+                chunk->GetValue(2, i).GetValue<int64_t>(),
+            };
+            data.push_back(entry);
         }
     }
 
@@ -80,29 +45,24 @@ int main()
     /**
      * Run the single-threaded version of groupby.
      */
-    std::unordered_map<duckdb::Value, std::vector<duckdb::Value>, ValueHash> record;
-    for (auto& row : data)
+    std::unordered_map<int64_t, Entry> record;
+    for (auto &row : data)
     {
         /**
          * Lookup the previous value.
          */
-        std::vector<duckdb::Value> prev;
-        if (auto search = record.find(row[0]); search != record.end())
+        Entry prev;
+        if (auto search = record.find(row.l_orderkey); search != record.end())
         {
             prev = search->second;
-        }
-        else
-        {
-            prev = std::vector<duckdb::Value>(vals.size(), duckdb::Value(0));
         }
         /**
          * Do the aggregation. WARNING: SUM for now.
          */
-        for (duckdb::idx_t i = 0; i < vals.size(); i++)
-        {
-            prev[i] = duckdb::Value(prev[i].GetValue<int64_t>() + row[1 + i].GetValue<int64_t>());
-        }
-        record[row[0]] = prev;
+        record[row.l_orderkey] = prev;
+        prev.l_orderkey = row.l_orderkey;
+        prev.l_partkey += row.l_partkey;
+        prev.l_suppkey += row.l_suppkey;
     }
 
     auto t1 = std::chrono::high_resolution_clock::now();
