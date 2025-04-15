00223bd21e89970736e09603736564c9871af2cc
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-04-15 08:44:47 -0400

    fmt

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,11 @@ int main() {
     duckdb::Connection con(db);
 
     /**
-    * Populate the data in row-major format with limited columns.
-    */
+     * Populate the data in row-major format with limited columns.
+     */
     std::vector<Entry> data;
     auto result =
-    con.Query("SELECT l_orderkey, l_partkey, l_suppkey FROM lineitem");
+        con.Query("SELECT l_orderkey, l_partkey, l_suppkey FROM lineitem");
     data.reserve(result->RowCount());
 
     while (auto chunk = result->Fetch()) {
@@ -40,20 +40,21 @@ int main() {
     auto t0 = std::chrono::high_resolution_clock::now();
 
     /**
-    * Run the single-threaded version of groupby.
-    */
+     * Run the single-threaded version of groupby.
+     */
     std::unordered_map<int64_t, Entry> agg_map;
     for (auto &row : data) {
         /**
-        * Lookup the previous value.
-        */
+         * Lookup the previous value.
+         */
         Entry prev;
-        if (auto search = agg_map.find(row.l_orderkey); search != agg_map.end()) {
+        if (auto search = agg_map.find(row.l_orderkey);
+            search != agg_map.end()) {
             prev = search->second;
         }
         /**
-        * Do the aggregation. WARNING: SUM for now.
-        */
+         * Do the aggregation. WARNING: SUM for now.
+         */
         agg_map[row.l_orderkey] = prev;
         prev.l_orderkey = row.l_orderkey;
         prev.l_partkey += row.l_partkey;
@@ -61,7 +62,10 @@ int main() {
     }
 
     auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << " ms" << std::endl;
+    std::cout << "Elapsed time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0)
+                     .count()
+              << " ms" << std::endl;
 
     return 0;
 }
