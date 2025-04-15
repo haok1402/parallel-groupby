0aedf64187ccc1910f43e47e73ddad50a1172ec4
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-04-15 10:11:17 -0400

    main exp skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include "duckdb/common/types/value.hpp"
 #include <chrono>
 #include <duckdb.hpp>
 #include <iostream>
 #include <omp.h>
+#include <string>
 
 struct Entry {
     int64_t l_orderkey;
@@ -9,7 +11,60 @@ struct Entry {
     int64_t l_suppkey;
 };
 
-int sequential_sol() {
+enum class AggFunc {
+    SUM,
+    AVG,
+    MIN,
+    MAX
+};
+
+enum class Strategy {
+    SEQUENTIAL,
+    TWO_PHASE_CENTRALIZED_MERGE,
+};
+
+// experiment config, including input file, what to group, what to aggregate, etc.
+class ExpConfig {
+public:
+    int num_threads;
+    Strategy strategy;
+    std::string in_db_file_path;
+    std::string group_key_col_name;
+    std::vector<std::string> data_col_names;
+    std::vector<AggFunc> agg_funcs; // TODO support diffeerent aggregation functions
+    
+    void display() {
+        std::cout << "exp config:" << std::endl;
+        std::cout << "num_threads = " << num_threads << std::endl;
+        std::cout << "strategy = " << ((int) strategy) << std::endl;
+        std::cout << "in_db_file_path = " << in_db_file_path << std::endl;
+        std::cout << "group_key_col_name = " << group_key_col_name << std::endl;
+        std::cout << "data_col_names = [";
+        for (const auto& col : data_col_names) {
+            std::cout << col << ", ";
+        }
+        std::cout << "]" << std::endl;
+        std::cout << "agg_funcs = [";
+        for (const auto& func : agg_funcs) {
+            std::cout << ((int) func) << ", ";
+        }
+        std::cout << "]" << std::endl;
+    }
+};
+
+// column major data storage
+class DataStore {
+public:
+    std::vector<std::vector<duckdb::Value>> data;
+    
+    duckdb::Value get(int row_idx, int col_idx) {
+        return data[col_idx][row_idx];
+    }
+    
+    
+};
+
+void sequential_sol(ExpConfig &config) {
     std::string table_name = "lineitem";
     std::string db_fpath = "data/tpch-sf1.db";
     std::vector<std::string> group_key_cols = {"l_orderkey"};
@@ -62,7 +117,6 @@ int sequential_sol() {
     auto t1 = std::chrono::high_resolution_clock::now();
     std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << " ms" << std::endl;
 
-    return 0;
 }
 
 void hello_omp() {
@@ -76,7 +130,22 @@ void hello_omp() {
     }
 }
 
-int main() {
-    hello_omp();
+int main(int argc, char *argv[]) {
+    
+    // 1 > parse command line
+    
+    // make a ExpConfig TODO parse from cli instead
+    ExpConfig config;
+    config.num_threads = 8;
+    config.strategy = Strategy::SEQUENTIAL;
+    config.in_db_file_path = "data/lineitem.tbl";
+    config.group_key_col_name = "l_orderkey";
+    config.data_col_names = {"l_partkey", "l_suppkey"};
+    config.agg_funcs = {AggFunc::SUM, AggFunc::SUM};
+    
+    // 2 > load the data
+    
+    // 3 > run the experiment
+    
     return 0;
 }
