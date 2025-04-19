7a874c5576479b3f39d2e37b43450857a9fdd2f4
haok1402 <haok1402@gmail.com>
2025-04-19 01:37:21 -0400

    modify the setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Author: Hao Kang
-# Date: April 14, 2025
-
 # Prepare the development environment.
 wget https://github.com/duckdb/duckdb/releases/download/v1.2.2/libduckdb-linux-amd64.zip
 unzip libduckdb-linux-amd64.zip && rm libduckdb-linux-amd64.zip
@@ -12,6 +9,9 @@ mv duckdb.h duckdb.hpp lib/duckdb/include/
 
 mkdir -p lib/duckdb/lib
 mv libduckdb.so libduckdb_static.a lib/duckdb/lib/
+
+mkdir -p lib/CLI11/include/
+wget -O lib/CLI11/include/CLI11.hpp https://github.com/CLIUtils/CLI11/releases/download/v2.5.0/CLI11.hpp
 
 python3 -m venv venv
 venv/bin/pip install -r requirements.txt
---
+++ src/generate.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "DEBUG" << std::endl;
+    return 0;
+}
