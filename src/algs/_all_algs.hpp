2ddffa0daafe5842e1b97c8eca96000ce4c267dd
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-04-27 14:40:05 -0400

    set up for new impls

--- src/algs/_all_algs.hpp
+++ src/algs/_all_algs.hpp
@@ -8,6 +8,7 @@
 void sequential_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void global_lock_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void two_phase_centralised_merge_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
+void two_phase_tree_merge_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void two_phase_centralised_merge_xxhash_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void duckdbish_two_phase_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void implicit_repartitioning_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
@@ -15,3 +16,4 @@ void three_phase_radix_sol(ExpConfig &config, RowStore &table, int trial_idx, bo
 void two_phase_radix_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void two_phase_radix_xxhash_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
 void lock_free_hash_table_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
+void omp_lock_free_hash_table_sol(ExpConfig &config, RowStore &table, int trial_idx, bool do_print_stats, std::vector<AggResRow> &agg_res);
